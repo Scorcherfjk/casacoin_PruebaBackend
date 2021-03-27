86af3a09c958e86f0defad273c8707b0ed806130
argierincon <argierincon@gmail.com>
2021-03-27 10:00:25 -0500

    changes on the model

--- api/models.py
+++ api/models.py
@@ -3,10 +3,19 @@ from django.db import models
 
 class Scraper(models.Model):
 
-    currency = models.CharField(max_length=50)
-    value = models.CharField(max_length=50)
+    currency = models.CharField(max_length=50, unique=True)
+    value = models.IntegerField()
     frequency = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     value_updated_at = models.DateTimeField(auto_now=True)
 
+    def toDict(self):
+        return {
+            "id" : self.id,
+            "currency" : self.currency,
+            "value" : self.value,
+            "frequency" : self.frequency,
+            "created_at" : self.created_at,
+            "value_updated_at" : self.value_updated_at,
+        }
 
