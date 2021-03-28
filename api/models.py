50ad3bdd90b15f8dc7cb8649c9f6fe44aee69249
argierincon <argierincon@gmail.com>
2021-03-28 12:32:14 -0500

    final model

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Scraper(models.Model):
 
     currency = models.CharField(max_length=50, unique=True)
-    value = models.IntegerField()
+    value = models.FloatField(default=0)
     frequency = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     value_updated_at = models.DateTimeField(auto_now=True)
